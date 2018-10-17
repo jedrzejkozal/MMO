33ac1728d19e425a9c11ffaf553b6bab4174b06a
jedrzejkozal <jedrzej.kozal@gmail.com>
2018-10-17 18:04:48 +0200

    working version using dataset

--- main.m
+++ main.m
@@ -4,8 +4,8 @@ Nfindings = 6;
 Ndiseases = 2;
 
 N=Nfindings+Ndiseases;
-findings = Ndiseases+1:N;
-diseases = 1:Ndiseases;
+findings = 1:Nfindings;
+diseases = Nfindings+1:N;
 
 data = load_data();
 Ntraning = 100;
@@ -13,37 +13,22 @@ Nvalidation = length(data)-Ntraning;
 traning_data = data(1:Ntraning);
 validation_data = data(Ntraning+1:length(data));
 
-%generationg graph structure
+%generate graph structure
 G = ones(Ndiseases, Nfindings);
 
 prior = calc_prior(traning_data, Nfindings, Ndiseases);
 leak = 0.98 *ones(1,Nfindings); % in real QMR, leak approx exp(-0.02) = 0.98 
-inhibit = calc_inhibit(traning_data, Nfindings, Ndiseases);
-
+inhibit = calc_inhibit(traning_data, Nfindings, Ndiseases, prior);
 inhibit(not(G)) = 1;
 
-% first half of findings are +ve, second half -ve
-% The very first and last findings are hidden
-pos = 1:floor(Nfindings/2);
-neg = (pos(end)+1):(Nfindings);
-obs_nodes = myunion(pos, neg) + Ndiseases;
+obs_nodes = 1:N;
 
 % Make the bnet in the straightforward way
-tabular_leaves = 1;
+tabular_leaves = 1; %   = 0 means noisy-OR leaves
 bnet = mk_qmr_bnet(G, inhibit, leak, prior, tabular_leaves, obs_nodes);
-evidence = cell(1, N);
-evidence(findings(pos)) = num2cell(repmat(2, 1, length(pos)));
-evidence(findings(neg)) = num2cell(repmat(1, 1, length(neg)));
 
 engine = jtree_inf_engine(bnet);
-ll = 0;
 
-tic; [engine, ll] = enter_evidence(engine, evidence); toc
-
-post = zeros(1, Ndiseases);
-for i=diseases(:)'
-    m = marginal_nodes(engine, i);
-    post(1, i) = m.T(2);
-end
-
-post
+evidence_vector = validation_data{11};
+post = calc_posterior(evidence_vector, engine, diseases, Ndiseases, Nfindings);
+[post evidence_vector(diseases)]
